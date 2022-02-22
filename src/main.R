8ae2526cce5923cdaae0b0b6c4f3bebbc946692f
insp3ctre <aschwarz@lclark.edu>
2022-02-22 20:46:06 +0000

    reorganizing repository

---
+++ src/main.R
@@ -0,0 +1,19 @@
+# Adam Schwarz, Katie Crocker, Katelyn Osborne  
+# LemmonHeads 
+
+# load libraries
+library("spocc")
+library("sp")
+library("raster")
+library("maptools")
+library("rgdal")
+library("dismo")
+library("sf")
+library("tidyverse")
+library("tidyr")
+
+# clean data #
+
+
+
+# generate map #
