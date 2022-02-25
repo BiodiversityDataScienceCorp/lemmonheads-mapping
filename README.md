06f80cf2b60ad028091603c5b88a06bc2ab76d4d
insp3ctre <aschwarz@lclark.edu>
2022-02-25 01:36:27 +0000

    migrate code to main.R and work on main README

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# lemmonheads-mapping
+# **Species Status Assessment (SSA)**
+
+## Species Name:
+### Lemmon's Milkweed
+
+## Species Taxonomy:
+### Asclepias Lemmonii
+
+## Species Description:
+### 
+
+## Habitat Description:
+### 
+
+## Larval Host Information:
+### 
+
+## Data Source for Occurence and Distribution Modeling:
+### [GBIF](https://www.gbif.org/species/3170287)
+
+## ![Species Occurrence Map](/cloud/project/output/Assignment5_LemmonHeads.jpg)
+
+## [Link to GitHub Respository](https://github.com/BiodiversityDataScienceCorp/lemmonheads-mapping)
+
+## References:
+### [GBIF](https://www.gbif.org/species/3170287)
+### [NatureServe Explorer](https://explorer.natureserve.org/Taxon/ELEMENT_GLOBAL.2.131047/Asclepias_lemmonii)
+### [SEINet](https://swbiodiversity.org/seinet/taxa/index.php?taxon=3763)
+### [eol](https://eol.org/pages/586616)
+### [USFWS](https://www.fws.gov/southwest/es/Documents/R2ES/Pollinators/8-Milkweeds_Handbook_XerSoc_June2014.pdf)
--- src/main.R
+++ src/main.R
@@ -1,6 +1,8 @@
 # Adam Schwarz, Katie Crocker, Katelyn Osborne  
 # LemmonHeads 
 
+#####
+
 # load libraries
 library("spocc")
 library("sp")
@@ -12,12 +14,82 @@ library("sf")
 library("tidyverse")
 library("tidyr")
 
-# load functions from functions.R
+#####
+
+# load functions from functions.R #
 source("src/functions.R")
 
+#####
+
+# pull data #
+  # group member 1
+lemmonData <- occ(query='Asclepias lemmonii A.Gray', from='gbif', limit=1000)
+lemmonData <- lemmonData$gbif$data$Asclepias_lemmonii_A.Gray
+  
+  # group member 2
+lemmonData2 <- occ(query='Asclepias lemmonii A.Gray', from='gbif', limit=1000)
+lemmonData2 <- lemmonData2$gbif$data$Asclepias_lemmonii_A.Gray
+
+  # group member 3
+lemmonData3 <- occ(query='Asclepias lemmonii A.Gray', from='gbif', limit=1000)
+
+#####
 
 # clean data #
+  # no "ABSENT" data
+unique(lemmonData$occurrenceStatus)
+absent <- subset(x=lemmonData, occurrenceStatus != "PRESENT")
+lemmonData <- anti_join(lemmonData, absent)
+lemmonData
+
+  # no zero counts
+unique(lemmonData$individualCount)
+zeroMilkweed <- subset(x=lemmonData, individualCount==0)
+lemmonData <- anti_join(lemmonData, zeroMilkweed)
+lemmonData
+
+  # remove missing data
+lemmonData <- lemmonData %>% filter(longitude != 'NA')
+lemmonData
+
+#####
+
+# mapping #
+
+# max and min latitude
+max.lat <- ceiling(max(lemmonData$latitude))
+min.lat <- floor(min(lemmonData$latitude))
+
+# max and min longitude
+max.lon <- ceiling(max(lemmonData$longitude))
+min.lon <- floor(min(lemmonData$longitude))
+
+# save as jpg
+jpeg(file="Assignment5_LemmonHeads.jpg")
+
+# load spatial polygons
+data(wrld_simpl)
+
+# plot base map
+plot(wrld_simpl, 
+     xlim = c(min.lon, max.lon), # sets upper/lower x
+     ylim = c(min.lat, max.lat), # sets upper/lower y
+     axes = TRUE, 
+     col = "grey95",
+     main="Lemmon's Milkweed species occurrences in Arizona and Mexico",  # title
+     sub="131 occurrences of Lemmon's Milkweed" # caption
+)
+
+# add the points for individual observation
+points(x = lemmonData$longitude, 
+       y = lemmonData$latitude, 
+       col = "blue", 
+       pch = 20, 
+       cex = 0.75)
+
+# draw a little box around the graph
+box()
+# stop mapping
+dev.off()
 
 
-
-# generate map #
